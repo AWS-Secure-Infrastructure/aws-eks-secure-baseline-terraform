79d187c609a50ae64d6288b3b1d7f9ccbc047007
SebaSilC <sebastian@playbookvisualarts.com>
2026-02-16 17:15:19 +0100

    Update main.tf

--- terraform/modules/security/main.tf
+++ terraform/modules/security/main.tf
@@ -9,3 +9,25 @@ resource "kubernetes_namespace" "secure_apps" {
     }
   }
 }
+
+resource "kubernetes_network_policy" "default_deny" {
+  metadata {
+    name      = "default-deny"
+    namespace = kubernetes_namespace.secure_apps.metadata[0].name
+  }
+
+  spec {
+    pod_selector {}
+
+    policy_types = ["Ingress", "Egress"]
+  }
+}
+
+resource "helm_release" "gatekeeper" {
+  name       = "gatekeeper"
+  repository = "https://open-policy-agent.github.io/gatekeeper/charts"
+  chart      = "gatekeeper"
+  namespace  = "gatekeeper-system"
+
+  create_namespace = true
+}
