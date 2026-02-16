52ed5af43b13e5de4c69f329c4cc293a7f7a2611
SebaSilC <sebastian@playbookvisualarts.com>
2026-02-16 17:09:45 +0100

    Update main.tf

--- terraform/environments/prod/main.tf
+++ terraform/environments/prod/main.tf
@@ -2,3 +2,10 @@ module "vpc" {
   source       = "../../modules/vpc"
   project_name = var.project_name
 }
+
+module "eks" {
+  source       = "../../modules/eks"
+  project_name = var.project_name
+  vpc_id       = module.vpc.vpc_id
+  subnet_ids   = module.vpc.public_subnet_ids
+}
