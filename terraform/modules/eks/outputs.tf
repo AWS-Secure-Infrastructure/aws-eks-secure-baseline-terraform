3cb04d1d73250049ac2e9fac9adeedb9cc8c7245
SebaSilC <sebastian@playbookvisualarts.com>
2026-02-16 17:11:29 +0100

    Update outputs.tf

--- terraform/modules/eks/outputs.tf
+++ terraform/modules/eks/outputs.tf
@@ -13,3 +13,7 @@ output "cluster_certificate_authority_data" {
 output "oidc_provider_arn" {
   value = aws_iam_openid_connect_provider.this.arn
 }
+
+output "oidc_provider_url" {
+  value = aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
