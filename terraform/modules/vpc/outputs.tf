3b1fca3587df256d5c56c2d4a8db0216a586107a
SebaSilC <sebastian@playbookvisualarts.com>
2026-02-16 17:19:44 +0100

    Update outputs.tf

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -1,5 +1,8 @@
-output "vpc_id" {
-  value = aws_vpc.this.id
+output "private_subnet_ids" {
+  value = [
+    aws_subnet.private_a.id,
+    aws_subnet.private_b.id
+  ]
 }
 
 output "public_subnet_ids" {
