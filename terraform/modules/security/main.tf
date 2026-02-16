0bf62d30f17b74d490db55b74de87e55de5a0bec
SebaSilC <sebastian@playbookvisualarts.com>
2026-02-16 17:29:36 +0100

    Update main.tf

--- terraform/modules/security/main.tf
+++ terraform/modules/security/main.tf
@@ -1,3 +1,5 @@
+# Secure Namespace (Pod Security Admission - restricted)
+
 resource "kubernetes_namespace" "secure_apps" {
   metadata {
     name = "secure-apps"
@@ -9,6 +11,8 @@ resource "kubernetes_namespace" "secure_apps" {
     }
   }
 }
+
+# Default Deny Network Policy
 
 resource "kubernetes_network_policy" "default_deny" {
   metadata {
@@ -23,34 +27,17 @@ resource "kubernetes_network_policy" "default_deny" {
   }
 }
 
-resource "helm_release" "gatekeeper" {
-  name       = "gatekeeper"
-  repository = "https://open-policy-agent.github.io/gatekeeper/charts"
-  chart      = "gatekeeper"
-  namespace  = "gatekeeper-system"
+# OPA Gatekeeper Installation
 
+resource "helm_release" "gatekeeper" {
+  name             = "gatekeeper"
+  repository       = "https://open-policy-agent.github.io/gatekeeper/charts"
+  chart            = "gatekeeper"
+  namespace        = "gatekeeper-system"
   create_namespace = true
 }
 
-resource "aws_iam_role" "example_irsa_role" {
-  name = "example-irsa-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Federated = module.eks.oidc_provider_arn
-      }
-      Action = "sts:AssumeRoleWithWebIdentity"
-      Condition = {
-        StringLike = {
-          "${replace(module.eks.oidc_provider_url, "https://", "")}:sub" = "system:serviceaccount:secure-apps:example-sa"
-        }
-      }
-    }]
-  })
-}
+# Constraint Template (Require CPU & Memory Limits)
 
 resource "kubernetes_manifest" "constraint_template" {
   manifest = {
@@ -90,6 +77,8 @@ EOF
 
   depends_on = [helm_release.gatekeeper]
 }
+
+# Constraint Instance
 
 resource "kubernetes_manifest" "require_limits" {
   manifest = {
